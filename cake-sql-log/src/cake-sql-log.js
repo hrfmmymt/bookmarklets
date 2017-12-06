64cdb5806965e5acd2afedf9093a348eedb8bebd
hrfmmymt <hrfmmymt@gmail.com>
2017-12-06 11:50:38 +0900

    modified

--- cake-sql-log/src/cake-sql-log.js
+++ cake-sql-log/src/cake-sql-log.js
@@ -1,35 +1,36 @@
 ((el, body) => {
   body = el.getElementsByTagName('head')[0];
-  const css = '.skyticket-cake-sql-log {\n' +
+  const CONST_VALUE = 'my-cake-sql-log';
+  const css = '.' + CONST_VALUE + '{\n' +
               '  width: 85%;\n' +
               '  margin: 40px auto;\n' +
               '  border-collapse: collapse;\n' +
               '  font-size: inherit;\n' +
               '}\n' +
-              '.skyticket-cake-sql-log th,\n' +
-              '.skyticket-cake-sql-log td {\n' +
+              '.' + CONST_VALUE + ' th,\n' +
+              '.' + CONST_VALUE + ' td {\n' +
               '  border: 1px solid #979797;\n' +
               '  padding: 5px;\n' +
               '}\n' +
-              '.skyticket-cake-sql-log th {\n' +
+              '.' + CONST_VALUE + ' th {\n' +
               '  background-color: #f7f7f7;\n' +
               '  font-weight: bold;\n' +
               '  text-align: center;\n' +
               '}\n' +
-              '.skyticket-cake-sql-log td:first-of-type {\n' +
+              '.' + CONST_VALUE + ' td:first-of-type {\n' +
               '  text-align: center;\n' +
               '}';
   const cakeSqlLogElements = document.getElementsByClassName('cake-sql-log');
-  if(el.getElementById('forSkyticketsCakeSqlLog')) {
-    body.removeChild(el.getElementById('forSkyticketsCakeSqlLog'));
+  if(el.getElementById(CONST_VALUE)) {
+    body.removeChild(el.getElementById(CONST_VALUE));
     Array.prototype.forEach.call(cakeSqlLogElements, node => {
-      node.classList.remove('skyticket-cake-sql-log');
+      node.classList.remove(CONST_VALUE);
     });
   } else {
     Array.prototype.forEach.call(cakeSqlLogElements, node => {
-      node.classList.add('skyticket-cake-sql-log');
+      node.classList.add(CONST_VALUE);
     });
     const styleEl = el.createElement('style');
-    styleEl.id = 'forSkyticketsCakeSqlLog', styleEl.appendChild(el.createTextNode(css)), body.appendChild(styleEl);
+    styleEl.id = CONST_VALUE, styleEl.appendChild(el.createTextNode(css)), body.appendChild(styleEl);
   }
 })(document);
